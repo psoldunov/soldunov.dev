a626177d94e22c583d642ba09b486be5d6684390
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2025-12-04 17:46:22 +0200

    feat: implement Container component and update Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import Container from '@/components/layout/container';
+
 export default function Home() {
-	return <main></main>;
+	return (
+		<main>
+			<section className='pt-10'>
+				<Container>
+					<h1 className='font-semibold text-3xl'>Philipp Soldunov</h1>
+					<p>
+						Software engineer specialized in building (and occasionally
+						designing) exceptional digital experiences.
+					</p>
+				</Container>
+			</section>
+		</main>
+	);
 }
---
+++ src/components/layout/container.tsx
@@ -0,0 +1,17 @@
+import { cn } from '@/lib/utils';
+
+export default function Container({
+	className,
+	children,
+}: {
+	className?: string;
+	children?: React.ReactNode;
+}) {
+	return (
+		<div className='px-6 md:px-8 xl:px-12'>
+			<div className={cn('mx-auto w-full max-w-7xl', className)}>
+				{children}
+			</div>
+		</div>
+	);
+}
